5cfec963a750d3208fbaca197e186bb2859ec0bc
Christoph Baudson <christoph.baudson@rewe-digital.com>
2023-12-06 09:26:57 +0100

    Day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,25 +1,39 @@
-import java.util.*
-
 fun main() {
 
+    fun part1(input: List<String>): Int {
+        val (t, d) = input
+        val time = t.substring(5).split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+        val distance = d.substring(9).split(" ").filter { it.isNotBlank() }.map { it.toInt() }
 
-    fun part1(input: List<String>): Int = TODO()
+        return time.mapIndexed { index, t ->
+            (0..t).count { speed ->
+                (t - speed) * speed > distance[index]
+            }
+        }.reduce { acc, i -> acc * i }
+    }
 
+    fun part2(input: List<String>): Int {
+        val (t, d) = input
+        val time = t.substring(5).split(" ").joinToString("").toLong()
+        val distance = d.substring(9).split(" ").joinToString("").toLong()
 
-    fun part2(input: List<String>): Int = TODO()
+        return (0L..time).count { speed ->
+            (time - speed) * speed > distance
+        }
+    }
 
     // test if implementation meets criteria from the description, like:
     val testInput1 = readInput("Day06_test")
     part1(testInput1).println()
-    check(part1(testInput1) == TODO())
+    check(part1(testInput1) == 288)
 
     val input = readInput("Day06")
     part1(input).println()
-    check(part1(input) == TODO())
+    check(part1(input) == 293046)
 
     part2(testInput1).println()
-    check(part2(testInput1) == TODO())
+    check(part2(testInput1) == 71503)
 
     part2(input).println()
-    check(part2(input) == TODO())
+    check(part2(input) == 35150181)
 }
